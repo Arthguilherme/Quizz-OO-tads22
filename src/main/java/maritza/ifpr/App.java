e56f8c7f93bc3cceed19131b1810088de8fdc6d5
arthguilherme <arth.ssg@gmail.com>
2024-08-28 09:08:37 -0300

    salvo

--- src/main/java/maritza/ifpr/App.java
+++ src/main/java/maritza/ifpr/App.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
@@ -51,6 +52,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+   
+        // FXMLLoader FXMLLoader= new FXMLLoader(getClass().getResource("layout.fxml"));
+       
+        // Parent root = FXMLLoader.load();
+       // Scene scene = new Scene(root,400,600);
 
         inicializaComponentes();
         atualizaComponentes();
@@ -58,8 +64,8 @@ public class App extends Application {
         cena = new Scene(root, 700, 500);
         cena.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
-        stage.setScene(cena);
-        stage.show();
+         stage.setScene(cena);
+         stage.show();
 
     }
 
--- src/main/java/maritza/ifpr/AppFile.java
+++ src/main/java/maritza/ifpr/AppFile.java
@@ -8,47 +8,45 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class AppFile {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        String pergunta;
-        String respostaCorreta;
-        String respostaErrada1;
-        String respostaErrada2;
-        String respostaErrada3;
-        String respostaErrada4;
+        String pergunta = "";
+        String respostaCorreta = "";
+        String respostaErrada1 = "";
+        String respostaErrada2 = "";
+        String respostaErrada3 = "";
+        String respostaErrada4 = "";
         int qtdePerguntas;
         ArrayList<Questao> questoes = new ArrayList<Questao>();
-        
+
         File arq = new File("Perguntas.txt");
-        
+
         System.out.println("Quantas perguntas voce quer fazer?");
         qtdePerguntas = ler.nextInt();
-        ler.nextLine();
 
-        while (qtdePerguntas > 0){ {
-            
+        while (qtdePerguntas > 0) {
+
             System.out.println("Digite sua pergunta");
-            pergunta = ler.nextLine();
+            pergunta = ler.next();
 
             System.out.println("Digite a resposta correta");
-            respostaCorreta = ler.nextLine();
+            respostaCorreta = ler.next();
             System.out.println("Agora digite as respostas erradas");
-            respostaErrada1 = ler.nextLine();
+            respostaErrada1 = ler.next();
             System.out.println("A segunda resposta");
-            respostaErrada2 = ler.nextLine();
+            respostaErrada2 = ler.next();
             System.out.println("A terceira resposta");
-            respostaErrada3 = ler.nextLine();
+            respostaErrada3 = ler.next();
             System.out.println("E a quarta resposta");
-            respostaErrada4 = ler.nextLine();
-            String[] respostasErradas = {respostaErrada1, respostaErrada2, respostaErrada3, respostaErrada4};
+            respostaErrada4 = ler.next();
+            String[] respostasErradas = { respostaErrada1, respostaErrada2, respostaErrada3, respostaErrada4 };
             Questao questao = new Questao(pergunta, respostaCorreta, respostasErradas);
             questoes.add(questao);
             qtdePerguntas--;
+            System.out.println("passou aqui");
         }
-
-        
+         
         // System.out.println("Essas sao as respostas: ");
         // System.out.printf("%s ",respostaCorreta);
         // System.out.printf("\n%s ",respostaErrada1);
@@ -56,39 +54,37 @@ public class AppFile {
         // System.out.printf("\n%s ",respostaErrada3);
         // System.out.printf("\n%s ",respostaErrada4);
 
-        try (FileWriter writer = new FileWriter("Perguntas.txt", true)) { // 'true' para adicionar ao final do arquivo, não sobrescrever
-        writer.write("Pergunta: " + pergunta + "\n");
-        writer.write(" " + respostaCorreta + "\n");
-        writer.write(" " + respostaErrada1 + "\n");
-        writer.write(" " + respostaErrada2 + "\n");
-        writer.write(" " + respostaErrada3 + "\n");
-        writer.write(" " + respostaErrada4 + "\n");
-        
-        writer.write("\n"); 
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        ler.close();
-    }
+        try (FileWriter writer = new FileWriter("Perguntas.txt", true)) { // 'true' para adicionar ao final do arquivo,
+                                                                          // não sobrescrever
+            writer.write("Pergunta: " + pergunta + "\n");
+            writer.write(" " + respostaCorreta + "\n");
+            writer.write(" " + respostaErrada1 + "\n");
+            writer.write(" " + respostaErrada2 + "\n");
+            writer.write(" " + respostaErrada3 + "\n");
+            writer.write(" " + respostaErrada4 + "\n");
 
-    try (BufferedReader reader = new BufferedReader(new FileReader("Perguntas.txt"))) {
-        String linha;
-        while ((linha = reader.readLine()) != null) {
-            System.out.println(linha);
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            ler.close();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("Perguntas.txt"))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Digite a pergunta
+        // Digite a resposta correta
+        // Digite 4 respostas erradas
+        // salve essas informaçoes em arquivos.
+        // Bonus: Pergunte ao usuário quantas perguntas ele quer cadastrar,
+        // e coloque a opção de digitar 0 para encerrar o cadastro
     }
 
-    
-
-
-    //Digite a pergunta
-    //Digite a resposta correta
-    //Digite 4 respostas erradas
-    //salve essas informaçoes em arquivos.
-    // Bonus: Pergunte ao usuário quantas perguntas ele quer cadastrar,
-    //e coloque a opção de digitar 0 para encerrar o cadastro
-    }
-    
-}}
+}
